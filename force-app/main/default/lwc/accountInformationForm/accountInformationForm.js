3a62979baabe8a7df29ad5b3e5b30db1e17b58e0
Craftorss <gukofficial@mail.ru>
2021-09-27 02:02:55 +0300

    lwc learning

---
+++ force-app/main/default/lwc/accountInformationForm/accountInformationForm.js
@@ -0,0 +1,6 @@
+import { api, LightningElement } from 'lwc';
+
+export default class AccountInformationForm extends LightningElement {
+    @api
+    account
+}
--- force-app/main/default/lwc/updateCompanyInformation/updateCompanyInformation.js
+++ force-app/main/default/lwc/updateCompanyInformation/updateCompanyInformation.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement, track } from 'lwc';
+import { CloseActionScreenEvent } from 'lightning/actions'
 
-export default class UpdateCompanyInformation extends LightningElement {}
+export default class UpdateCompanyInformation extends LightningElement {
+    account = {
+        name:'AccNAME', 
+        billingStreet:'Michigan Avenue here we are',
+        biilingPostalCode:'60601',
+        billingState:'IL',
+        billingCountry:'USA'
+    }
+
+
+    closeAction(){
+        this.dispatchEvent(
+            new CloseActionScreenEvent()
+        );
+    }
+}
