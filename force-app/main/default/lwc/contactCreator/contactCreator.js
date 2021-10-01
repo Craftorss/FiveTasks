af26e3e94d47ec07c08a9d77805f34852dd8f862
Craftorss <gukofficial@mail.ru>
2021-10-01 17:53:54 +0300

    trails on ma way

---
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -0,0 +1,20 @@
+import { LightningElement } from 'lwc';
+import FIRST_NAME from '@salesforce/schema/Contact.FirstName';
+import LAST_NAME from '@salesforce/schema/Contact.LastName';
+import EMAIL from '@salesforce/schema/Contact.Email';
+import CONTACT_OBJECT from '@salesforce/schema/Contact'
+
+const fields = [FIRST_NAME, LAST_NAME, EMAIL]
+const objectApiName = CONTACT_OBJECT.objectApiName
+export default class ContactCreator extends LightningElement {
+    fields = fields
+    objectApiName = objectApiName
+
+    handleSuccess(event){
+        this.dispatchEvent(new ShowToastEvent({
+            title: 'Contact created',
+            message: 'Record Id: ' + event.detail.id,
+            variant: 'success'
+        }));
+    }
+}
